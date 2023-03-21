91c789d5e0785c106620d65013e5d4178476b1af
WadeFade <math44100@yahoo.fr>
2023-03-21 20:49:46 +0100

    need correction on beautifulsoup

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 from fastapi import FastAPI
 
+from request import get_current
+
 app = FastAPI()
+
+base_url: str = 'https://edtmobiliteng.wigorservices.net//WebPsDyn.aspx?action=posEDTBEECOME&serverid=i'
 
 
 # https://calendar.lightin.io/v1/week?firstname=breval&lastname=lefloch
+
+
 @app.get("/v1/month")
 async def get_edt_month(firstname: str, lastname: str):
-    return {"firstname": firstname, "lastname": lastname}
+    # return await get_html_content(base_url, firstname, lastname, '2023-01-01')
+    return await get_current(firstname, lastname)
+
+
+
